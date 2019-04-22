1f00bed0265ec7eeef5756ad8bb8c219f361e16e
Hector Garcia <hectoregarciap@hotmail.com>
2019-04-22 11:39:23 -0500

    componente button

--- src/components/InputTextFmb.js
+++ src/components/InputTextFmb.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-import "../css/InputTextFmb.css"
-
+import "../css/InputTextFmb.css";
 
 class InputTextFmb extends Component {
-    render() {
-        return(            
-                  
-            <div className="form-group has-search">
-                <span className="fa fa-search form-control-feedback"></span>
-                <input type="text" className="form-control" placeholder="Search" />
-            </div>
-
-
-          );
-        }
+  render() {
+    return (
+      <div className="form-group has-search">
+        <span className="fa fa-search form-control-feedback" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          value={this.props.value}
+          onChange={this.props.onChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default InputTextFmb;
-
-    
