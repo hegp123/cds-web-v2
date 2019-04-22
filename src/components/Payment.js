0c6346c9f8ead37793f10c19e4fac72bedbb6d5a
MAYASOFT120\carlos.delgado <carfenan1@gmail.com>
2019-04-22 16:05:08 -0500

    asd

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -11,43 +11,43 @@ import "../css/Payment.css";
 class Payment extends Component {
 
 
-    render(){
+    render() {
         return (
-           
-<div>
 
-    <div className="navbar navbar-default navbar-fixed-top app" >
-      <div className="container">
-        <div className="navbar-header">
-         
-          <a className="navbar-brand" href="#">Project name</a>
-        </div>       
-      </div>
-    </div>
+            <div>
 
-   
-    <div className="container, containerFmb">
-      <div className="page-header">
-        <h1>Sticky footer with fixed navbar</h1>
-      </div>
-      <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
-      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
-    </div>
+                <div className="navbar navbar-default navbar-fixed-top app" >
+                    <div className="container">
+                        <div className="navbar-header">
 
-    <footer>
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-4"> <img src={payment}  /></div>
-                <div className="col-sm-4"> <img src={invoice}  /></div>
-                <div className="col-sm-4"> <img src={report}  /></div>
+                            <a className="navbar-brand" href="#">Project name</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="container, containerFmb">
+                    <div className="page-header">
+                        <h1>Sticky footer with fixed navbar</h1>
+                    </div>
+                    <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
+                    <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+                </div>
+
+                <footer>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-4"> <img src={payment} /></div>
+                            <div className="col-sm-4"> <img src={invoice} /></div>
+                            <div className="col-sm-4"> <img src={report} /></div>
+                        </div>
+
+
+                    </div>
+                </footer>
+
+
             </div>
-
-        <p className="text-muted">Place sticky footer content here.</p>
-      </div>
-    </footer>
-
-
-</div>
 
         );
     }
