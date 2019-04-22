d2eab0854945c8abbc5619e4b86f783ba6a8af39
MAYASOFT120\carlos.delgado <carfenan1@gmail.com>
2019-04-22 15:55:27 -0500

    a

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './components/Login';
+import Payment from './components/Payment';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Payment />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
