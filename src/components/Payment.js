e74f39002ac27e261d73bd44dee1c68ab4d49398
Hector Garcia <hectoregarciap@hotmail.com>
2019-04-23 11:08:19 -0500

    componente content

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import HeaderFmb from "./HeaderFmb";
 import FooterFmb from "./FooterFmb";
+import PaymentContainer from "./PaymentContainer";
 
 import "../css/Payment.css";
 
@@ -10,21 +11,8 @@ class Payment extends Component {
     return (
       <div>
         <HeaderFmb type={typeProcess} />
-        <div className="container, containerFmb">
-          <div className="page-header">
-            <h1>Sticky footer with fixed navbar</h1>
-          </div>
-          <p className="lead">
-            Pin a fixed-height footer to the bottom of the viewport in desktop
-            browsers with this custom HTML and CSS. A fixed navbar has been
-            added with <code>padding-top: 60px;</code> on the{" "}
-            <code>body > .container</code>.
-          </p>
-          <p>
-            Back to <a href="../sticky-footer">the default sticky footer</a>{" "}
-            minus the navbar.
-          </p>
-        </div>
+
+        <PaymentContainer />
 
         <FooterFmb type={typeProcess} />
       </div>
