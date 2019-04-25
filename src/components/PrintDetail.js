12db35c82438c8eb271d30029b76720509c5808a
MAYASOFT120\carlos.delgado <carfenan1@gmail.com>
2019-04-25 14:53:08 -0500

    a

--- src/components/PrintDetail.js
+++ src/components/PrintDetail.js
@@ -1,3 +1,20 @@
 import React, { Component } from "react";
+import HeaderFmb from "./HeaderFmb";
 
-class Print extends Component {}
+class PrintDetail extends Component {
+  render() {
+    const typeProcess = "print";
+    return (
+      <div>
+        <HeaderFmb type={typeProcess} />
+        <div className="container">
+          <div className="row, text-center">
+            <b>Reimpresion</b>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PrintDetail;
