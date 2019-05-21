a8d515f6d0c4792a5bfb44039d03a025679df8e5
MAYASOFT120\carlos.delgado <carfenan1@gmail.com>
2019-05-21 16:31:17 -0500

    a

--- src/components/ReportContainer.js
+++ src/components/ReportContainer.js
@@ -19,7 +19,7 @@ class ReportContainer extends Component {
     super(props);
     this.state = {
       startDate: new Date(),
-      modalReport: "",
+      modalReport: false,
       modalAlert: false,
       collectionPoint: "",
       totalPaid: "",
